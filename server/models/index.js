8ef9e5ed7cee0ee7df482f04aa52cde5bd64b017
github username <github email address>
2018-12-18 14:53:49 -0500

    wired up graphql and created App and SongList components

--- server/models/index.js
+++ server/models/index.js
@@ -1,2 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.plugin(schema => {
+  schema.options.usePushEach = true;
+});
+
 require('./song');
 require('./lyric');
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://forrest:password123@ds237574.mlab.com:37574/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
